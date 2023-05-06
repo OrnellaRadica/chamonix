7e9bc2ee976f7a0254a1a0afa663652f74f8de26
Ornella Radica <ornellaradica@MacBook-Pro-de-Ornella.local>
2023-05-06 13:40:07 +0200

    Create footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="border-t border-zinc-100">
+      <div className="mx-auto px-4 py-6 sm:px-6 lg:px-8 lg:max-w-7xl">
+      <p className="text-sm text-zinc-400 font-light">By Ornella Radica</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
